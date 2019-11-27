eb3c76e6908bd1d0358e150d52dfe3a5727ee072
Killer74 <killerjohan73@gmail.com>
2019-11-27 11:56:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-pbzxn7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-pbzxn7)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  name = "Angular";
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOninit() {
+     debugger;
+    this.getNews().subscribe(response => {
+      console.log(response); 
+    });
+  }
+
+  public getNews() {
+   
+    var url = "https://jsonplaceholder.typicode.com/todos/";
+    return this.httpClient.get(url);
+  }
+}
